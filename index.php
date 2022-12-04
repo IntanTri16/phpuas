cc17dcb0efdb6b850b50261aba2d1605640526a9
Intan Tri Isnasari <112366319+IntanTri16@users.noreply.github.com>
2022-12-04 11:03:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+    <header>
+    <div class="header">
+        <div class="header-logo"><b>ARMY<span>BTS</span></b></div>
+            <div class="header-list">
+                <dl>
+                    <dt><a href="https://www.instagram.com/bts.bighitofficial/?theme=dark">CONTACT US</a></dt>
+                    <dt><a href="https://ibighit.com/bts/eng/discography/">ARCHIVE</a></dt>
+                    <dt><a href="https://www.vlive.tv/tags/RUNBTS">RUN BTS</a></dt>
+                    <dt><a href="https://www.youtube.com/watch?v=38k5zr1e0HI&ab_channel=BTS-Topic">MAGIC SHOP</a></dt>
+                    <dt><a href="https://ibighit.com/bts/eng/">BTS</a></dt>
+                </dl>
+            </div>
+        </div>
+    </div>
+    </header>
+
+    <div class="top-wrapper">
+        <div class="top">
+            <h1>WELCOME TO MAGIC SHOP<spam>.</spam></h1>
+            <h2>"Days when you hate that you’re you. Days when you wanna disappear. Lets make a door in your heart. <br>
+                If you open that door and go inside, this place will be waiting for you. It is ok to believe, it wll comfort you, this Magic Shop." (Magic Shop, LY:Tear, 2018)</h2>
+        </div>
+    </div>
+
+    <main>
+    </div>
+    <div class="main">
+        <div class="title">
+            <h1>BTS &#128156 ARMY</h1>
+            <h2>Let's join to our &#127775 MAGIC SHOP &#127775</h2>
+            <h3 class="section-title">&#128147 BELOVED MEMBERS &#128147</h3>
+        </div>
+
+        <div class="contents">
+            
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/ba/1c/ed/ba1cedb24f0bee9563f80d36cce4cf96.jpg" height="250px">
+                <p><a href="https://www.instagram.com/rkive/"><b>KIM NAMJOON</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/af/e4/1b/afe41b47de29d953c9aad5959391101b.jpg" height="250px">
+                <p><a href="https://www.instagram.com/agustd/"><b>MIN YOONGI</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/77/1a/24/771a2436f5ee29264f9e9b2f69f11913.jpg" height="250px">
+                <p><a href="https://www.instagram.com/jin/"><b>KIM SEOKJIN</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/d5/c6/ac/d5c6ac9b5a6507e37d8498d88563a8c7.jpg" height="250px">
+                <p><a href="https://www.instagram.com/uarmyhope/"><b>JUNG HOSOEK</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/7a/95/81/7a9581cf83999e583aaf84131cd705ae.jpg" height="250px">
+                <p><a href="https://www.instagram.com/j.m/"><b>KIM TAEHYUNG</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/736x/3e/4c/3b/3e4c3b97b63159d9bc37e4e2d02728e1.jpg" height="250px">
+                <p><a href="https://www.instagram.com/thv/"><b>PARK JIMIN</b></a></p>
+            </div>
+
+            <div class="contents-item">
+                <img src="https://i.pinimg.com/564x/cc/f2/e8/ccf2e87ad1a0c10cec414436be9045b2.jpg" height="250px">
+                <p><a href="https://www.instagram.com/jungkook.97/"><b>JEON JUNGKOOK</b></a></p>
+            </div>
+        </div>
+
+        <div class="contact-form">
+            <h3 class="section-footer">CONTACT US</h3>
+            <form action="welcome.php" method="post">
+                First Name: <input type="text" name="firstname"><br>
+                Last Name: <input type="text" name="lastname"><br>
+                Email: <input type="text" name="email"><br>
+                <input type="submit">
+            </form>
+           
+        </div>
+        
+       
+        
+
+    </div>
+    </main>
+
+    <footer>
+    <div class="footer">
+        <div class="footer-list">
+            <ul>
+                <li>BIGHIT MUSIC</li>
+                <li>HYBE CORPORATION</li>
+                <li>Yongsan, Seoul, Korea Selatan</li>
+            </ul>
+        </div>
+    </div>
+    </footer>
+
+</body>
+</html>
